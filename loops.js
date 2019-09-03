4ba802af6a46fa6709bee3dd3cb789958c5561d2
Barak Saidoff <barak.saidoff@gmail.com>
2019-09-03 20:15:50 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,31 +1,34 @@
 //for loop function 
-function forLoop(array){
-for (i = 0; i < 25; i++) {
-if(array[i]===1){
-array.push("I am 1 strange loop.");
-} else {
-array.push(`I am ${i} strange loops.`)
-}
-}
-return array;
+
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
+  }
+  return array;
 }
 
 // while loop function 
 function whileLoop(number){
-  while (number > 0){
-    console.log(--number);
-    
+  let countdown=number;
+  while (countdown > 0){
+    console.log(--countdown);
   }
+  return "done"
 }
+
 function incrementVariable() {
-  i = i + 1;
-  return i;
+  return i + 1;
 }
-// do while function
+
 function doWhileLoop(number){
 do { console.log("I run once regardless.")
-}while (incrementVariable()<number){
-  
+}
+while (incrementVariable()<number){
+  return console.log("I run once regardless.")
 }
 }
 
